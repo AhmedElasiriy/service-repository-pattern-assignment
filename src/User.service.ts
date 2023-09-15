2404489eff889b8f695d5ad254bab9224348ebc1
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-09-16 00:36:05 +0300

    Tests: All user service tests passing

--- src/User.service.ts
+++ src/User.service.ts
@@ -2,4 +2,9 @@ import { UserRequestDto } from './Database'
 import { UserRepository } from './User.repository'
 
 // NOTE: Make userRepo public and not private. This is required for the tests!
-export class UserService {}
+export class UserService {
+  public userRepo = new UserRepository()
+  createUser(userName: UserRequestDto) {
+    return this.userRepo.createUser(userName)
+  }
+}
